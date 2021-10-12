3c0f0a6882b1bfa714a98600946a488efd070f27
HelleAndrew <43493370+HelleAndrew@users.noreply.github.com>
2021-10-13 01:29:00 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int numberOfApples=12;
+          decimal pricePerApple=0.35M;
+          Console.WriteLine(
+              format:"{0} apples costs {1:C}.",  
+              arg0:numberOfApples, 
+              arg1:numberOfApples*pricePerApple
+          );
+
+          string formatted=string.Format(
+              format:"{0} apples costs {1:C}.",  
+              arg0:numberOfApples, 
+              arg1:numberOfApples*pricePerApple
+          );
+
+          Console.Write(formatted);
+
+        }
+    }
+}
